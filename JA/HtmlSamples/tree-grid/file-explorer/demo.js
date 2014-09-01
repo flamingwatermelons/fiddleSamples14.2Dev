0a296af690ec2fd1323b88429babf6d1885f66ac
Ignite UI Samples Browser deployer <igniteui.sb@gmail.com>
2014-09-01 10:41:04 -0400

    SB build 14.2.20142.47

--- JA/HtmlSamples/tree-grid/file-explorer/demo.js
+++ JA/HtmlSamples/tree-grid/file-explorer/demo.js
@@ -2,45 +2,45 @@ $(function () {
 
             var files = [
                 {
-                    "Name": "Documents", "DateModified": "9/12/2013", "Type": "File Folder", "Size": "4,480 KB", "Files": [
-                        { "Name": "To do list.txt", "DateModified": "11/30/2013", "Type": "TXT File", "Size": "4,448 KB" },
-                        { "Name": "Work.txt", "DateModified": "9/12/2013", "Type": "TXT File", "Size": "32 KB" }
+                    "Name": "Documents", "DateModified": "9/12/2013", "Type": "File Folder", "Size": "4480", "Files": [
+                        { "Name": "To do list.txt", "DateModified": "11/30/2013", "Type": "TXT File", "Size": "4448" },
+                        { "Name": "Work.txt", "DateModified": "9/12/2013", "Type": "TXT File", "Size": "32" }
                     ]
                 },
                 {
-                    "Name": "Music", "DateModified": "6/10/2014", "Type": "File Folder", "Size": "5,594 KB", "Files": [
+                    "Name": "Music", "DateModified": "6/10/2014", "Type": "File Folder", "Size": "5594", "Files": [
                         {
-                            "Name": "AC/DC", "DateModified": "6/10/2014", "Type": "File Folder", "Size": "2,726 KB", "Files": [
-                                { "Name": "Stand Up.mp3", "DateModified": "6/10/2014", "Type": "MP3 File", "Size": "456 KB" },
-                                { "Name": "T.N.T.mp3", "DateModified": "6/10/2014", "Type": "MP3 File", "Size": "1,155 KB" },
-                                { "Name": "The Jack.mp3", "DateModified": "6/10/2014", "Type": "MP3 File", "Size": "1,115 KB" }
+                            "Name": "AC/DC", "DateModified": "6/10/2014", "Type": "File Folder", "Size": "2726", "Files": [
+                                { "Name": "Stand Up.mp3", "DateModified": "6/10/2014", "Type": "MP3 File", "Size": "456" },
+                                { "Name": "T.N.T.mp3", "DateModified": "6/10/2014", "Type": "MP3 File", "Size": "1155" },
+                                { "Name": "The Jack.mp3", "DateModified": "6/10/2014", "Type": "MP3 File", "Size": "1115" }
                             ]
                         },
                         {
-                            "Name": "WhiteSnake", "DateModified": "6/11/2014", "Type": "File Folder", "Size": "2,868 KB", "Files": [
-                                { "Name": "Trouble.mp3", "DateModified": "6/11/2014", "Type": "MP3 File", "Size": "1,234 KB" },
-                                { "Name": "Bad Boys.mp3", "DateModified": "6/11/2014", "Type": "MP3 File", "Size": "522 KB" },
-                                { "Name": "Is This Love.mp3", "DateModified": "6/11/2014", "Type": "MP3 File", "Size": "1,112 KB" },
+                            "Name": "WhiteSnake", "DateModified": "6/11/2014", "Type": "File Folder", "Size": "2868", "Files": [
+                                { "Name": "Trouble.mp3", "DateModified": "6/11/2014", "Type": "MP3 File", "Size": "1234" },
+                                { "Name": "Bad Boys.mp3", "DateModified": "6/11/2014", "Type": "MP3 File", "Size": "522" },
+                                { "Name": "Is This Love.mp3", "DateModified": "6/11/2014", "Type": "MP3 File", "Size": "1112" },
                             ]
                         }
                     ]
                 },
                 {
-                    "Name": "Pictures", "DateModified": "1/20/2014", "Type": "File Folder", "Size": "1,825 KB", "Files": [
+                    "Name": "Pictures", "DateModified": "1/20/2014", "Type": "File Folder", "Size": "1825", "Files": [
                         {
-                            "Name": "Jack's Birthday", "DateModified": "6/21/2014", "Type": "File Folder", "Size": "631 KB", "Files": [
-                                { "Name": "Picture1.png", "DateModified": "6/21/2014", "Type": "PNG image", "Size": "493 KB" },
-                                { "Name": "Picture2.png", "DateModified": "6/21/2014", "Type": "PNG image", "Size": "88 KB" },
-                                { "Name": "Picture3.gif", "DateModified": "6/21/2014", "Type": "GIF File", "Size": "50 KB" },
+                            "Name": "Jack's Birthday", "DateModified": "6/21/2014", "Type": "File Folder", "Size": "631", "Files": [
+                                { "Name": "Picture1.png", "DateModified": "6/21/2014", "Type": "PNG image", "Size": "493" },
+                                { "Name": "Picture2.png", "DateModified": "6/21/2014", "Type": "PNG image", "Size": "88" },
+                                { "Name": "Picture3.gif", "DateModified": "6/21/2014", "Type": "GIF File", "Size": "50" },
                             ]
                         },
                         {
-                            "Name": "Trip to London", "DateModified": "3/10/2014", "Type": "File Folder", "Size": "1,194 KB", "Files": [
-                                { "Name": "Picture1.png", "DateModified": "3/10/2014", "Type": "PNG image", "Size": "974 KB" },
-                                { "Name": "Picture2.png", "DateModified": "3/10/2014", "Type": "PNG image", "Size": "142 KB" },
-                                { "Name": "Picture3.png", "DateModified": "3/10/2014", "Type": "PNG image", "Size": "41 KB" },
-                                { "Name": "Picture4.png", "DateModified": "3/10/2014", "Type": "PNG image", "Size": "25 KB" },
-                                { "Name": "Picture5.png", "DateModified": "3/10/2014", "Type": "PNG image", "Size": "12 KB" },
+                            "Name": "Trip to London", "DateModified": "3/10/2014", "Type": "File Folder", "Size": "1194", "Files": [
+                                { "Name": "Picture1.png", "DateModified": "3/10/2014", "Type": "PNG image", "Size": "974" },
+                                { "Name": "Picture2.png", "DateModified": "3/10/2014", "Type": "PNG image", "Size": "142" },
+                                { "Name": "Picture3.png", "DateModified": "3/10/2014", "Type": "PNG image", "Size": "41" },
+                                { "Name": "Picture4.png", "DateModified": "3/10/2014", "Type": "PNG image", "Size": "25" },
+                                { "Name": "Picture5.png", "DateModified": "3/10/2014", "Type": "PNG image", "Size": "12" },
                             ]
                         }
                     ]
@@ -49,17 +49,18 @@ $(function () {
 
             $("#treegrid").igTreeGrid({
                 width: "800px",
+                height:"400px",
                 dataSource: files,
                 autoGenerateColumns: false,
                 primaryKey: "Name",
                 columns: [
                     { headerText: "Name", key: "Name", width: "250px", dataType: "string" },
-                    { headerText: "Date Modified", key: "DateModified", width: "130px", dataType: "string" },
+                    { headerText: "Date Modified", key: "DateModified", width: "130px", dataType: "date" },
                     { headerText: "Type", key: "Type", width: "130px", dataType: "string" },
-                    { headerText: "Size", key: "Size", width: "130px", dataType: "string" },
+                    { headerText: "Size", key: "Size", width: "130px", dataType: "number", template: "${Size} KB" },
                 ],
                 dataSourceLayoutKey: "Files",
-                initialExpandDepth: 1,
+                initialExpandDepth: 2,
                 renderExpandColumn: true,
                 features: [
                 {
